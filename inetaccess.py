54c76f9b1d66b49cd5f52dead522d3575863afd5
jstanford2013 <74340273+jstanford2013@users.noreply.github.com>
2021-03-29 20:05:13 -0500

    Revert "Merge branch 'master' of https://github.com/jeredbare/paradigm"
    This reverts commit fe1a376a0ae394825c64c8fd1631b235a8c3096a, reversing
    changes made to 9151b4bd47d88f4cd62cee2839d0bb2669f21e45.

--- inetaccess.py
+++ inetaccess.py
@@ -13,24 +13,22 @@ http_status_code=[]
 https_status_code = []
 
 
-# opens the Amass file to read the JSON.  The JSON module in python does not like the output, hence telling it to read line by line.
+
 for line in open('[amass_json_file]', 'r'):
     targets.append(json.loads(line))
 
-# converting the data into lists
 for i in targets:
     fqdn.append(i['name'])
     domain.append(i['name'])
     ip.append(i['domain'])
 
-    # converting the addresses into a list.  
     for a in i['addresses']:
         ip.append(a['ip'])
         cidr.append(a['cidr'])
         loc.append(a['desc'])
 
 
-# this for loop will test to see if the domain is open to the internet.  
+
 for i in fqdn:
     try:
         http = requests.get("http://{}".format(i), timeout=2)
@@ -44,11 +42,4 @@ for i in fqdn:
         http = "No HTTP Response"
         https = "No HTTPS Response"
         continue
-
-# storing the data into a dictionary  
 dict = {'fqdn': fqdn, 'domain': domain, 'ip': ip, 'cidr': cidr, 'location': loc, 'http': http_status_code, 'https': https_status_code}
-
-# TO DO
-# CSV?
-# Storing in a DB?
-# DOCKER?
