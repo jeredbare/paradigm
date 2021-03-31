779a839a4dbf45720caafffc63c4eee4cad568cc
jstanford2013 <74340273+jstanford2013@users.noreply.github.com>
2021-03-30 21:10:35 -0500

    updated to json return
    Updated so the http response is returned regardless if it's a 200 or not BUT it's only counted towards the scoring if the response is a 200.
    
    Broke out the build json dict building into it's own function to avoid duplicate code

--- inetaccess.py
+++ inetaccess.py
@@ -2,33 +2,37 @@ import requests
 import json
 
 
+def build_response_dict(build_dict, target, http, https):
+    build_dict['fqdn'] = target['name']
+    build_dict['domain'] = target['domain']
+    build_dict['ip'] = target['addresses'][0]['ip']
+    build_dict['cidr'] = target['addresses'][0]['cidr']
+    build_dict['loc'] = target['addresses'][0]['desc']
+    build_dict['http'] = http
+    build_dict['https'] = https
+    return build_dict
+
+
 def scan_site(file_to_scan):
     targets = []
     ok_responses = 0
     json_response = {}
     scan_data = []
-    response_dict = {'fqdn': '', 'domain': '', 'ip': [], 'cidr': '',
-                     'loc': [], 'http': '', 'https': ''}
 
     for line in open(file_to_scan, 'r'):
         targets.append(json.loads(line))
 
     for target in targets:
+        response_dict = {'fqdn': '', 'domain': '', 'ip': [], 'cidr': '', 'loc': [], 'http': '', 'https': ''}
         try:
-            http = requests.get("http://{}".format(target['name']), timeout=1)
-            https = requests.get("https://{}".format(target['name']), timeout=1)
+            http = requests.get("http://{}".format(target['name']), timeout=2)
+            https = requests.get("https://{}".format(target['name']), timeout=2)
+            scan_data.append(build_response_dict(response_dict, target, str(http.status_code), str(https.status_code)))
             if http.status_code != 200 or https.status_code != 200:
                 continue
-            response_dict['fqdn'] = target['name']
-            response_dict['domain'] = target['domain']
-            response_dict['ip'] = target['addresses'][0]['ip']
-            response_dict['cidr'] = target['addresses'][0]['cidr']
-            response_dict['loc'] = target['addresses'][0]['desc']
-            response_dict['http'] = str(http.status_code)
-            response_dict['https'] = str(https.status_code)
-            scan_data.append(response_dict)
             ok_responses = ok_responses + 1
         except requests.exceptions.ConnectionError as e:
+            scan_data.append(build_response_dict(response_dict, target, "No Response", "No Response"))
             continue
 
     result_score = (ok_responses / len(targets)) * 100
