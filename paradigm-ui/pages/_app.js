d61d87dfd2e3f1b9ea6646d3e82495df870031b8
jstanford2013 <74340273+jstanford2013@users.noreply.github.com>
2021-07-24 18:19:08 -0500

    Pulling in upload card into dashboard
    Fix-> Spacing on fileupload card is off with the div of the side bar.

--- paradigm-ui/pages/_app.js
+++ paradigm-ui/pages/_app.js
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
+import withDarkMode, { MODE } from 'next-dark-mode'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default withDarkMode(MyApp, { defaultMode: MODE.DARK })
--- paradigm-ui/pages/dashboard.tsx
+++ paradigm-ui/pages/dashboard.tsx
@@ -3,12 +3,20 @@ import Box from '@material-ui/core/Box';
 import { ProSidebar, Menu, MenuItem, SubMenu, SidebarHeader } from 'react-pro-sidebar';
 import 'react-pro-sidebar/dist/css/styles.css';
 import Typography from '@material-ui/core/Typography'
-import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
-import HelpIcon from '@material-ui/icons/Help';
 import React, { useState, useEffect } from 'react';
 import { withStyles } from "@material-ui/core/styles";
+import  FileUpload  from '../components/fileUpload'
+import AmassDataTable from '../components/amassDataTable'
+import ScanScore from '../components/scanScore'
+import CircularProgress from "@material-ui/core/CircularProgress";
+import ScanResultTable from '../components/scanResultTable'
+import Button from '@material-ui/core/Button';
 
-export default function Dashboard() {
+export default function Dashboard(data) {
+    useEffect(() => {
+        document.body.style.backgroundColor = "#121212";
+    },[])
+
     const WhiteTextTypography = withStyles({
         root: {
         color: "#FFFFFF",
@@ -16,6 +24,28 @@ export default function Dashboard() {
         }
     })(Typography);   
     
+    const [fileContent, setFileContent] = useState(null)
+    const [scanResults, setScanResults] = useState(null)
+    const [isScanning, setIsScanning] = useState(false)
+
+    const getScanScore = async() => {
+        setIsScanning(true)
+    
+        const response = await fetch("/api/corsPost?url=http://paradigm:5000/scan-site",
+        {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(fileContent)
+        })
+    
+        const result = await response.json()
+        setIsScanning(false)
+        setScanResults(result)
+      }
+
     return (
         <Box display="flex" flexDirection="row" height="100%">
             <ProSidebar style={{position: "absolute", height: "100%"}}>
@@ -33,6 +63,24 @@ export default function Dashboard() {
                 </SubMenu>
                 </Menu>
             </ProSidebar>
+                {fileContent ?
+            <Box width="100%" display="flex" flexDirection="column" style={{ justifyContent: "space-evenly" }}>
+                <Box width="100%" display="flex" flexDirection="row" style={{ justifyContent: "space-evenly" }}>
+                <FileUpload fileUpdate={setFileContent}/>
+                <Box height="50%" display="flex" flexDirection="column" style={{ justifyContent: "space-between" }}>
+                    <Button variant="contained"onClick={()=> {getScanScore()}}>Get Score</Button>
+                    <p></p>
+                    <Button variant="contained"onClick={()=> {setFileContent(null)}}>Clear Data</Button>
+                </Box>
+                {scanResults ? <ScanScore scanScore={scanResults.result_score}/> : null}
+                {isScanning ? <CircularProgress style={{color: '#FFFFFF'}} /> : null}
+                </Box>
+                <p></p>
+                {scanResults ? <ScanResultTable scanResults={scanResults}/> : null}
+                <p></p>
+                <AmassDataTable fileContent={fileContent}/>
+            </Box>
+            : <FileUpload fileUpdate={setFileContent}/>}
         </Box>
          
   )
